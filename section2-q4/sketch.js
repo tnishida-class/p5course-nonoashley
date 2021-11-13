24f518558438195ed00ac41db4aa33de70c8f875
nonoashley <92346056+nonoashley@users.noreply.github.com>
2021-11-13 23:49:01 +0900

    change 2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,25 +1,21 @@
 // ギリシャ国旗
 function setup() {
-  const blue = color(29, 94, 174);
+  const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  noStroke();// noStroke();
+  // noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
-    if( i % 2 == 0 ){
-      fill(blue)
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, d);
+    rect(0, i * d, width, (i + 1) * d);
   }
 
   fill(blue);
   let size = d * 5;
-  rect( 0 , 0 , size , size );
+  rect(0, 0, size, size);
 
   fill(255);
-  rect( d*2 , 0 , d , size)
-  rect( 0 , d*2 , size , d )// BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
-}
+  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
