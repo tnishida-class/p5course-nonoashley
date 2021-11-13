3eea811befe97974ec71fb2e03d1211e08d9d6d9
nonoashley <92346056+nonoashley@users.noreply.github.com>
2021-11-13 23:49:10 +0900

    change 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -3,24 +3,14 @@ let count;
 let cycle;
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(200, 200);
   count = 0;
-  cycle = 50;
+  cycle = 100;
 }
 
 function draw(){
-  background(160, 192, 255);  count = (count + 1) % cycle;
-    if(count<30){
-      ellipse(width / 2, height / 2, count+20); }
-    if(count>30){
-      ellipse(width / 2, height / 2, 120-2*count);}
-
-  if(keyIsPressed){
-    count = (count + 1) % cycle;
-    if(count<15){
-          ellipse(width / 2, height / 2, 2*count+20); }
-          if(count>15){
-            ellipse(width / 2, height / 2, 120-2*count)
-          }
-}
+  background(160, 192, 255);
+  count = (count + 1) % cycle;
+  // BLANK[1]
+  ellipse(width / 2, height / 2, size);
 }
