fd77022d9d834ac66a7e8706b80a91a429eacd71
nonoashley <92346056+nonoashley@users.noreply.github.com>
2021-11-13 23:48:37 +0900

    change 2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,39 +1,11 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width / 0;
+  let size = width / 8;
   noStroke();
-  for(let i = 0 ; i < 8; i++){
-    for(let j = 0 ; j < 8; j++){
-      fill((i + j) % 2 == 1 ? 128 : 255);
-      console.log(i, j); // 補足： i,j の変化がわかりやすくなるように入れています
-      rect(i * 25, j * 25, 25, 25);
-
-    }
-
-  for(let k = 0.5 ; k < 8 ; k++){
-   for(let l = 0.5 ; l < 8 ; l++){
-     if((k + l) % 2 == 0){
-        if(l<3)fill(255,0,0)
-        if(l>5)fill(0)
-     }
-     else{
-         noFill()
-       }
-     console.log(k, l);
-     ellipse( k * 25 , l * 25 , 20 )
-   }
- }
-}
-}
-
-
-
-
-
-
-
-}
-
-
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    }
+  }
+}
