ebf4a7fa551514b57a76c1664f2dfff1117ed983
nonoashley <92346056+nonoashley@users.noreply.github.com>
2021-11-25 04:51:38 +0900

    change 4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -19,6 +19,20 @@ function setup(){
   const dx = width / scores.length;
   let px, py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
-    // BLANK[1]
+    if( i > 0 ){
+      px = ( i - 1 ) * width / 10
+      py = scores[ i - 1 ] * height / 100
+      strokeWeight(1);
+      line(px, py,i*dx, scores[i] * height / 100 );
+      strokeWeight(10);
+      point(px,py);
+    }// BLANK[1]
+    if(i==0){
+  strokeWeight(1);
+  line(i * dx, scores[i] * height / 100, (i+1)*dx, scores[i+1] * height / 100 )
+  strokeWeight(10);
+  point(i * dx, scores[i] * height / 100)
+
+    }
   }
 }
