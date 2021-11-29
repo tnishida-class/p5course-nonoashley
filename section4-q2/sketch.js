81fb7d83ccc9d88734e373fc4dc6c077cd472683
nonoashley <92346056+nonoashley@users.noreply.github.com>
2021-11-29 23:57:17 +0900

    change 4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -9,10 +9,21 @@ function setup(){
   for(let i = 0; i < 10; i++){
     scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
   }
-
+  console.log(scores);
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
+  for(let i = 0; i < scores.length; i++){ total += scores[i]
+  console.log(total)
 
-  // BLANK[1]
+   // BLANK[1]
+
+if( i == 0 ){
+  arc(200,200,350,350, -HALF_PI , (scores[i] * Math.PI / 20 / total), PIE)
 }
+if( i > 0 ){
+  arc(200,200,350,350, -HALF_PI , (scores[i] * Math.PI / 20 / total), PIE );
+
+  }
+}
+}
+//どうすれば前の地点から角度を出せるのかがわからない
